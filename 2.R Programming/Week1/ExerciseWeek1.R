f94ab2bdf0291f62fce62e8719de37c06cb72cdd
mina jm <iyanim90@gmail.com>
2021-03-26 11:51:02 +0000

    R programming Week 2, First Commit

--- 2.R Programming/Week1/ExerciseWeek1.R
+++ 2.R Programming/Week1/ExerciseWeek1.R
@@ -1,1 +1,12 @@
+#1. Install swirl
+install.packages("swirl")
+packageVersion("swirl")
 
+#2. Load swirl
+library(swirl)
+
+#3. Install the R Progroamming course
+install_from_swirl("R Programming")
+
+#4. Start swirl and complete the lessons
+swirl()
